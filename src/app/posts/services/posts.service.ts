3b2ca9d0e89b8151bc6f3e3f668f85993dc5114d
rafal.bromboszcz <rafal.bromboszcz@softiq.pl>
2019-10-03 14:32:52 +0200

    Change location of news

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IPostList } from 'src/app/shared/interfaces/post-list.interface';
 import { sortByOperator } from 'src/app/shared/helpers/sorter.helper';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -14,7 +15,7 @@ export class PostsService {
   ) { }
 
   async getPosts() {
-    const url = 'assets/fake-posts.json'
+    const url = environment.postsUrl
     let list = await this.http.get<IPostList>(url)
     .pipe(
       sortByOperator('createdTime')
