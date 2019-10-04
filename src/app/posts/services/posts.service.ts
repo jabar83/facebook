44d0fcdba0d605c8c6dae7a485ae528fa7b169f6
rafal.bromboszcz <rafal.bromboszcz@softiq.pl>
2019-10-04 13:42:15 +0200

    Loading screen

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IPostList } from 'src/app/shared/interfaces/post-list.interface';
-import { sortByOperator } from 'src/app/shared/helpers/sorter.helper';
+import { sortByOperator, sortBy } from 'src/app/shared/helpers/sorter.helper';
 import { environment } from 'src/environments/environment';
 import { find } from 'rxjs/operators';
 import { IPost } from 'src/app/shared/interfaces/post.interface';
@@ -19,10 +19,11 @@ export class PostsService {
   async getPosts(): Promise<IPostList> {
     const url = environment.postsUrl
     let list = await this.http.get<IPostList>(url)
-    .pipe(
-      sortByOperator('createdTime')
-      )
+    // .pipe(
+    //   sortByOperator('createdTime')
+    //   )
     .toPromise();
+    sortBy<IPostList>(response, (post)=> new Date(post.))
     return list;
 
   }
